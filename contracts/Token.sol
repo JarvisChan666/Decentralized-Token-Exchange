8bb714c98480d4b3a2ade0e5e1eec26c6f0cf653
JarvisChan666 <744765386@qq.com>
2023-06-12 18:33:00 +0800

    2. smoke test

---
+++ contracts/Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.9;
+
+import "../node_modules/hardhat/console.sol";
+
+contract Token {
+    string public name = "My Token";
+
+}
+
+
---
+++ scripts/1_deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+const { isCompositeComponent } = require("react-dom/test-utils");
+
+async function main() {
+  //fetch contract to deploy
+    const Token = await ethers.getContractFactory("Token")
+
+  //deploy contract
+    const token = await Token.deploy()
+    await token.deployed()
+    console.log(`Token deployed to ${token.address}`)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
