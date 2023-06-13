d4ebddd5b6c913d76380d8fd74802a18c23f9959
JarvisChan666 <744765386@qq.com>
2023-06-13 21:15:09 +0800

    4. token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,15 +11,43 @@ contract Token {
 
     //track balance put address, and show balance number
     mapping(address => uint256) public balanceOf;
+    
+    event Transfer(
+        address indexed from , 
+        address indexed to, 
+        uint256 value 
+    );
+
     //send token
 
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+    constructor
+    (string memory _name, string memory _symbol, uint256 _totalSupply
+    ) {
         name = _name; //Token.deploy('Jarvis')
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         //msg is global value,sender is the account who is deploying this contract?
         balanceOf[msg.sender] = totalSupply;//mapping use []
+    }
+
+    function transfer(address _to, uint256 _value) 
+        public 
+        returns (bool success) 
+    {
+        //require that sender have enough token to spend
+        require(balanceOf[msg.sender] >= _value);
+        
+
+        // deduct token from spender
+        balanceOf[msg.sender]  = balanceOf[msg.sender] - _value;
+        //credit token to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+        
+        //emit event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
 
--- test/Token.js
+++ test/Token.js
@@ -11,7 +11,7 @@ const tokens = (n) => { //Arrow functions are convenient defining short function
 
 
 describe('Token', () => {
-    let token, accounts, deployer
+    let token, accounts, deployer, receiver
 
     beforeEach(async () => {
         //execuit first
@@ -19,7 +19,8 @@ describe('Token', () => {
         token = await Token.deploy('Jarvis', 'JVC', 1000000)//deploy it,get the name 
     
         accounts = await ethers.getSigners()
-        deployer = accounts[0];
+        deployer = accounts[0]
+        receiver = accounts[1]
     })
     describe('Deployment', () => {
         const name = 'Jarvis'
@@ -53,8 +54,47 @@ describe('Token', () => {
             //change ether to wei
             expect(await token.balanceOf(deployer.address)).to.equal(totalSupply) 
         })
+    })
+
+    describe('Sending Token', () => {
+
+        let amount, transaction, result
+
+        describe('Success', () => {
+            beforeEach(async () => {
+                amount = tokens(100)
+                let transaction = await token.connect(deployer).transfer(receiver.address, amount)
+                result = await transaction.wait()
+            })
+            it('Transfers token balances', async() => {
+               
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+                
+            })
     
+            it('Emit a transfer event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+               
+                const args = event.args
+                expect(args.from).to.equal(deployer.address)
+                expect(args.to).to.equal(receiver.address)
+                expect(args.value).to.equal(amount)
+            })
+        })
+        
+        describe('Failure', () => {
+            it('rejects insufficient balances' async () => {
+                //Transfer more tokens than deployer has - 10m
+                const invalideAmount = tokens(100000000)
+                await expect(token.connect(deployer).transfer(receiver.address, invalideAmount)).to.be.reverted
+            })
+        })
+
     })
 
 
 }) 
+
+
