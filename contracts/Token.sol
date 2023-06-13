3f434e5c4e257fbaa68aff23c7bdb00c427e8f05
JarvisChan666 <744765386@qq.com>
2023-06-13 19:57:36 +0800

    4. token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,14 +9,18 @@ contract Token {
     uint256 public decimals = 18;//dont change
     uint256 public totalSupply;
 
+    //track balance put address, and show balance number
+    mapping(address => uint256) public balanceOf;
+    //send token
+
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name; //Token.deploy('Jarvis')
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
+        //msg is global value,sender is the account who is deploying this contract?
+        balanceOf[msg.sender] = totalSupply;//mapping use []
     }
-
-
-
 }
 
 
--- test/Token.js
+++ test/Token.js
@@ -1,18 +1,25 @@
+
+const { TokenClass } = require("typescript")
 const { ethers } = require("hardhat")//import ethers from hardhat
 const { expect } = require("chai") 
 
 //change ether to wei
-const tokens = (n) => {
+const tokens = (n) => { //Arrow functions are convenient defining short functions.
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 
+
 describe('Token', () => {
-    let token 
+    let token, accounts, deployer
+
     beforeEach(async () => {
-    //execuit first
-    const Token = await ethers.getContractFactory('Token')
-    token = await Token.deploy('Jarvis', 'JVC', 1000000)//deploy it,get the name 
+        //execuit first
+        const Token = await ethers.getContractFactory('Token')
+        token = await Token.deploy('Jarvis', 'JVC', 1000000)//deploy it,get the name 
+    
+        accounts = await ethers.getSigners()
+        deployer = accounts[0];
     })
     describe('Deployment', () => {
         const name = 'Jarvis'
@@ -40,13 +47,14 @@ describe('Token', () => {
             //change ether to wei
             expect(await token.totalSupply()).to.equal(totalSupply) 
         })
+        
+        it('assign total supply to deployer', async () => {
+            //Check that name is correct
+            //change ether to wei
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply) 
+        })
     
     })
-
-    
-
-
-
 
 
 }) 
